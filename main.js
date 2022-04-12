718d4d0fd28f151ecea5baf5cf66075441842267
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-12 14:07:36 -0400

    Rotating the arrows based on the coords.heading that i recieve

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ const speed = document.querySelector('.speed-value')
 
 
 navigator.geolocation.watchPosition((data) => {
-  // console.log(data.coords.latitude)
   let lat = data.coords.latitude
   let long = data.coords.longitude
-  speed.textContent = Math.round(data.coords.speed * 0.62137)
-  console.log(lat)
-  console.log(long)
-  console.log(speed)
+  speed.textContent = (data.coords.speed * 0.62137).toFixed(2)
+  arrows.style.transform = `rotate(${data.coords.heading}deg)`
 })
