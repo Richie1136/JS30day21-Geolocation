07ba5f193268c9d8171553ba9e6b6eb724aef266
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-12 14:04:00 -0400

    Converting Kilometers to Miles

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const arrows = document.querySelector('.arrow')
-const speed = document.querySelector('.units')
+const speed = document.querySelector('.speed-value')
 
 
 
 navigator.geolocation.watchPosition((data) => {
-  console.log(data)
+  // console.log(data.coords.latitude)
+  let lat = data.coords.latitude
+  let long = data.coords.longitude
+  speed.textContent = Math.round(data.coords.speed * 0.62137)
+  console.log(lat)
+  console.log(long)
+  console.log(speed)
 })
